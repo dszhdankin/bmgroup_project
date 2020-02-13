5449b7c96fad909a3ea87d40bfb006b75f75892f
Mikle <1mikle1@mail.ru>
2020-02-14 01:54:05 +0300

    added ModelLesson

---
+++ Client/Version_1.0/Version_1.0/Model/ModelLesson.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace Version_1._0.Model
+{
+    public class Lesson
+    {
+        string name;
+        DateTime date;
+        string cabinetNumber;
+
+        public Lesson(string nam, string cab, DateTime dat)
+        {
+            name = nam;
+            cabinetNumber = cab;
+            date = dat;
+        }
+
+        public string Name
+        {
+            get => name;
+        }
+
+        public string Cabinet
+        {
+            get => cabinetNumber;
+        }
+
+        public string Date
+        {
+            get => date.ToString(System.Globalization.CultureInfo.InstalledUICulture);
+        }
+    }
+
+    class ModelLesson
+    {
+        public static ObservableCollection<Lesson> get(string url)
+        {
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
+            HttpWebResponse response = (HttpWebResponse)req.GetResponse();
+            if (response.StatusCode != HttpStatusCode.OK)
+            {
+                Console.WriteLine("StatusCode: " + response.StatusCode);
+                Console.WriteLine(response.StatusDescription);
+                return null;
+            }
+            StreamReader strm = new StreamReader(response.GetResponseStream());
+
+            string str = strm.ReadToEnd();
+
+            JObject joj = JObject.Parse("{ \"arr\":" + str + "}");
+
+            ObservableCollection<Lesson> list = new ObservableCollection<Lesson>();
+        
+            foreach (var token in joj.First.First)
+            {
+                list.Add(new Lesson((string)token["name"], (string)token["cabinet"], (DateTime)token["time"]));
+            }
+            return null;
+        }
+    }
+}
