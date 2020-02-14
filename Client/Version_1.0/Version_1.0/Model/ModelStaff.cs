1c187191c9eb1a945f0bd6c6ee4dec43978df128
Mikle <1mikle1@mail.ru>
2020-02-14 17:28:08 +0300

    added get request ModelStaff

--- Client/Version_1.0/Version_1.0/Model/ModelStaff.cs
+++ Client/Version_1.0/Version_1.0/Model/ModelStaff.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -28,5 +32,39 @@ namespace Version_1._0.Model
 
     public class ModelStaff
     {
+        public ObservableCollection<Teacher> get(string url)
+        {
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
+            HttpWebResponse response = (HttpWebResponse)req.GetResponse();
+            if (response.StatusCode != HttpStatusCode.OK)
+            {
+                Console.WriteLine("StatusCode: " + response.StatusCode);
+                Console.WriteLine(response.StatusDescription);
+                return null;
+            }
+            StreamReader strm = new StreamReader(response.GetResponseStream());
+
+            string str = strm.ReadToEnd();
+
+            JObject joj = JObject.Parse("{ \"arr\":" + str + "}");
+
+            var list = jsonParse(joj);
+
+            Console.WriteLine("Got events");
+
+            return list;
+        }
+
+        public ObservableCollection<Teacher> jsonParse(JObject joj)
+        {
+            var list = new ObservableCollection<Teacher>();
+
+            foreach (var token in joj.First.First)
+            {
+                list.Add(new Teacher((int)token["TeacherId"], (string)token["Name"], (string)token["Surname"],
+                    (string)token["Patronymic"], (byte[])token["Photo"], (string)token["Subjects"]));
+            }
+            return list;
+        }
     }
 }
