7503d8326d551c3ab7aa365089b9af3cf49a1a39
Mikle <1mikle1@mail.ru>
2020-02-15 03:09:58 +0300

    added Testing Project

---
+++ Client/Version_1.0/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Version_1._0.Model;
+
+namespace UnitTestFront
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
