5f3931eb64558ce128f03752fad0dfbd8d0c239a
Mikle <1mikle1@mail.ru>
2020-05-28 22:55:00 +0300

    same but not admin

--- Client/Version_1.0/Version_1.0/Model/ModelEvent.cs
+++ Client/Version_1.0/Version_1.0/Model/ModelEvent.cs
@@ -6,8 +6,9 @@ using System.Linq;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
-using Newtonsoft.Json.Linq;
+//using Newtonsoft.Json.Linq;
 using System.Web.Script.Serialization;
+using System.Windows;
 
 namespace Version_1._0.Model
 {
@@ -16,12 +17,14 @@ namespace Version_1._0.Model
         public int EventId { get; set; }
         public string Description { get; set; }
         public string Title { get; set; }
+
         public DateTime StartTime { get; set; }
+
         public string Photo { get; set; }
 
         public byte[] getBytePhoto()
         {
-            if (Photo != "QEA=")
+            if (Photo != null && Photo != "" && Photo != "QEA=")
                 return Encoding.Default.GetBytes(Photo);
             else
                 return null;
