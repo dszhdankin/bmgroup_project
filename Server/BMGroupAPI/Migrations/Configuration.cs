753c5f19cad1947c537a332651846799535065c4
Hofsiedge <zuev.ilia.al@yandex.ru>
2020-05-29 13:24:03 +0300

    DB Seed changed to set dates relative to current date

--- Server/BMGroupAPI/Migrations/Configuration.cs
+++ Server/BMGroupAPI/Migrations/Configuration.cs
@@ -15,17 +15,20 @@
 
         protected override void Seed(BMGroupAPI.Models.BMGroupAPIContext context)
         {
+
+            DateTime startDay = DateTime.Today;
+
             Event[] events = {
                     new Event() {
                         Description = "Additional lesson of math",
                         Title = "Only for bad students",
-                        StartTime = new DateTime(2020,6,7),
-                        Photo = null
+                        StartTime = startDay.AddDays(3).AddHours(19),
+                        Photo = null,
                     },
                     new Event() {
                         Description = "Additional lesson of language",
                         Title = "Only for good students",
-                        StartTime = new DateTime(2020,10,5),
+                        StartTime = startDay.AddDays(5).AddHours(17),
                         Photo = null
                     }
             };
@@ -33,54 +36,56 @@
             Ellective[] ellectives = {
                 new Ellective() {
                     Info = "QWERTY",
-                    Time = new DateTime(2020,10, 10, 10, 10, 10),
-                    Title = "Very Important Ellective"
+                    Time = startDay.AddDays(1).AddHours(10),
+                    Title = "Very Important Ellective",
                 },
                 new Ellective() {
                     Info = "ASDFGH",
-                    Time = new DateTime(2020,10, 10, 10, 10, 10),
-                    Title = "Not very Important Ellective"
+                    Time = startDay.AddDays(4).AddHours(14),
+                    Title = "Not very Important Ellective",
                 },
                 new Ellective() {
                     Info = "ZXCVBN",
-                    Time = new DateTime(2020,10, 10, 10, 10, 10),
-                    Title = "Usуless Ellective"
+                    Time = startDay.AddDays(10).AddHours(19),
+                    Title = "Useless Ellective",
                 }
             };
 
-            Lesson[] lessons = new Lesson[30];
 
-            for (int day = 0; day < 6; ++day)
+            const int DAYS = 20;
+            Lesson[] lessons = new Lesson[5 * DAYS];
+
+            for (int day = 0; day < DAYS; ++day)
             {
                 lessons[day * 5] = new Lesson()
                 {
                     ClassId = 1,
                     Info = "Math",
-                    Time = new DateTime(2020, 3 + day, 23, 9, 0, 0)
+                    Time = startDay.AddDays(day).AddHours(9),
                 };
                 lessons[day * 5 + 1] = new Lesson()
                 {
                     ClassId = 1,
                     Info = "English",
-                    Time = new DateTime(2020, 3 + day, 23, 10, 30, 0)
+                    Time = startDay.AddDays(day).AddHours(10).AddMinutes(30),
                 };
                 lessons[day * 5 + 2] = new Lesson()
                 {
                     ClassId = 1,
                     Info = "Programming",
-                    Time = new DateTime(2020, 3 + day, 23, 12, 0, 0)
+                    Time = startDay.AddDays(day).AddHours(12),
                 };
                 lessons[day * 5 + 3] = new Lesson()
                 {
                     ClassId = 1,
                     Info = "Programming",
-                    Time = new DateTime(2020, 3 + day, 23, 13, 30, 0)
+                    Time = startDay.AddDays(day).AddHours(13).AddMinutes(30),
                 };
                 lessons[day * 5 + 4] = new Lesson()
                 {
                     ClassId = 1,
                     Info = "Art",
-                    Time = new DateTime(2020, 3 + day, 23, 15, 0, 0)
+                    Time = startDay.AddDays(day).AddHours(15),
                 };
             }
             /*
